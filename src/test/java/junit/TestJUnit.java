8fabab5ecc3a12a3e5b3fa3ca2931e54162189e7
kimha <kimha@localhost>
2017-07-19 18:20:43 -0400

    initial commit for testing to GitHub

---
+++ src/test/java/junit/TestJUnit.java
@@ -0,0 +1,13 @@
+package junit;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class TestJUnit {
+	@Test
+	public void testSetup() {
+		String s = "junit test";
+		assertEquals("junit testing", s);
+	}
+}
